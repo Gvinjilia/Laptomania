f7efa9b520b29d18eb6d310ec4e44800a796ad13
Gvinjilia <gvinjilian2@gmail.com>
2025-11-24 20:54:00 +0400

    commit

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -4,27 +4,38 @@ const catchAsync = require("../utils/catchAsync");
 const jwt = require('jsonwebtoken');
 
 const protect = catchAsync(async (req, res, next) => {
-    const token = req.cookies.lg;
+    const token = req.cookies?.lg;
 
-    if(!token){
-        return next(new AppError('You are not logged in', 401))
-    };
+    try {
+        if(!token){
+            return next(new AppError('You are not logged in', 401))
+        };
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    if(!decoded){
-        return next(new AppError('Your token is invalid', 401))
+        if(!decoded){
+            return next(new AppError('Your token is invalid', 401))
+        }
+
+        const user = await User.findById(decoded.id);
+
+        if(!user){
+            return next(new AppError('User does not exist', 401))
+        };
+
+        req.user = user;
+
+        next();
+    } catch(error){
+        console.error("Auth Middleware Error:", error.message);
+
+        // Handle token expiration separately
+        if (error.name === "TokenExpiredError") {
+            return next(new AppError("you authorization time has expired", 401));
+        }
+
+        return next(new AppError("you are not authorized!", 401));
     }
-
-    const user = await User.findById(decoded.id);
-
-    if(!user){
-        return next(new AppError('User does not exist', 401))
-    };
-
-    req.user = user;
-
-    next();
 });
 
 module.exports = protect;
